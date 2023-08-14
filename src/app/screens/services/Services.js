2b5dbae3255185c34f277591c7b416e7b1e39c53
Bharath22012001 <bharatha22012001@gmail.com>
2023-08-14 15:30:43 +0530

    Services details

--- src/app/screens/services/Services.js
+++ src/app/screens/services/Services.js
@@ -4,9 +4,9 @@ import Footer from 'src/app/footer/Footer';
 import AppNavbar from 'components/AppNavbar';
 import Divider from 'components/base/Divider';
 import { OUR_SERVICES_LIST } from 'src/app/home/Home';
-import IMG4 from './img/img4.jpg';
-import IMG1 from './img/img1.jpg';
-import IMG2 from './img/img2.jpg';
+import IMG3 from './new-img/img3.jpg';
+import IMG1 from './new-img/img1.jpg';
+import IMG2 from './new-img/img2.jpg';
 import './Services.scss';
 import '../../index.scss';
 
@@ -26,7 +26,7 @@ export function Component() {
               fontSize={model.name === heading.name ? 18 : 16}
               paddingBottom={15}
               paddingLeft={15}
-              color={model.name === heading.name && 'black'}
+              color={model.name === heading.name && '#fd0200'}
             >
               {model.name}
             </Text>
@@ -44,51 +44,57 @@ export function Component() {
 
 export function ProductsCard() {
   const IMG_LIST = [
-    { img: IMG4, key: 'img4' },
-    { img: IMG1, key: 'img1' },
     { img: IMG2, key: 'img2' },
+    { img: IMG1, key: 'img1' },
+    { img: IMG3, key: 'img3' },
+
   ];
   const [img, setImg] = useState(IMG_LIST[0]);
   return (
     <div className="product-card-container">
       <div className="flex-column">
-        <img
-          alt=""
-          src={img.img}
-          style={{
-            borderRadius: '10px',
-            width: '220px',
-            height: '150px',
-            marginBottom: '15px',
-          }}
-        />
         <div className="flex">
+          <img
+            alt=""
+            src={img.img}
+            style={{
+              borderRadius: '8px',
+              width: '220px',
+              height: '150px',
+              marginBottom: '15px',
+            }}
+          />
+          <div
+            className="flex-column"
+            style={{
+              paddingLeft: '20px', height: '100%',
+            }}
+          >
+            <Heading paddingBottom={10} fontSize={20}>Product name</Heading>
+            <Text fontSize={16} paddingBottom={5}>Customer name</Text>
+            <Text fontSize={16}>
+              This is product description this is product description is product description
+            </Text>
+          </div>
+        </div>
+        <div className="flex" style={{ alignItems: 'center' }}>
           {IMG_LIST.map((model) => (
             <img
               alt=""
               src={model.img}
               style={{
-                borderRadius: '8px',
-                width: '50px',
-                height: '50px',
+                borderRadius: '5px',
+                width: model.key === img.key ? '60px' : '50px',
+                height: model.key === img.key ? '60px' : '50px',
                 marginRight: '15px',
+                // padding: '5px',
                 cursor: 'pointer',
-                border: model.key === img.key && '1px solid black',
+                border: model.key === img.key && '1px solid #fd0200',
               }}
               onClick={() => setImg(model)}
             />
           ))}
         </div>
-      </div>
-      <div
-        className="flex-column"
-        style={{
-          paddingLeft: '20px', height: '100%', justifyContent: 'center',
-        }}
-      >
-        <Heading paddingBottom={10} fontSize={20}>Product name</Heading>
-        <Text fontSize={16}>Customer name</Text>
-        <Text fontSize={16}>This is the product description</Text>
       </div>
     </div>
   );
